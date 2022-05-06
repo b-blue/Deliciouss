8282e609f05efcb81aa34f3fb849ea9b5a3a2d6e
Brandon Blue <brandon.ja.blue@gmail.com>
2022-05-06 11:26:42 -0500

    Changed category icon logic

---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+function Search() {
+  return <div></div>;
+}
+
+export default Search;
